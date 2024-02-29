7fbd2a19b982b4031518bbc3381d1be586a4c789
ali-b14 <alib714@gmail.com>
2024-02-29 13:52:25 -0500

    worked on Person class

---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,29 @@
+package object1;
+
+// Define a class
+class Person {
+    // Define Instance Variables
+    // different for each object!
+
+    String name;
+    int age = 0;
+    // units are in centimeters
+    int height;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.height = 0;
+    }
+
+    public static void main(String[] args) {
+        // Declare and initialize an object
+        Person person1 = new Person("John", 30);
+
+        int getHeight(int.height)
+
+        /* Now you can use the Object `person1` */
+
+    }
+}
